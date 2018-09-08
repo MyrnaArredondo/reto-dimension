be6ed192d0e958ae2fff91b50ffa0c9faf9dc290
MefitHp <mefitDev@gmail.com>
2018-09-08 18:43:19 -0500

    Dash

--- src/components/dash/DashboardContainer.js
+++ src/components/dash/DashboardContainer.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
 import { Breadcrumb, Icon, Row, Col } from 'antd'
-
+import { Line } from 'react-chartjs-2';
+const data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [{
+        label: "My First dataset",
+        // backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    },
+    {
+        label: "My Secon dataset",
+        // backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(150, 99, 50)',
+        data: [10, 1, 7, 45, 30, 20, 33],
+    }]
+}
 class DashboardContainer extends Component {
+
     render() {
         return (
             <div>
@@ -10,9 +26,10 @@ class DashboardContainer extends Component {
                     <Breadcrumb.Item>Dashboard [ NB_EMPRESA ]</Breadcrumb.Item>
                 </Breadcrumb>
                 <div style={{ padding: 24, background: '#fff' }}>
-                    <Row style={{ border: '1px solid goldenrod', padding: '8px', height: '350px', marginBottom: '10px' }}>
+                    <Row style={{ border: '1px solid goldenrod', padding: '8px', marginBottom: '10px' }}>
                         <Col span={24}>
                             <h1>Gráfica grande</h1>
+                            <Line data={data} width={1200} height={300} options={{ maintainAspectRatio: true, responsive: false, }} />
                         </Col>
                     </Row>
                     <Row gutter={8}>
